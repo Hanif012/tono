642bb17ca36051ce3299562456dba23964e29862
Hanif012 <andque12345012@gmail.com>
2023-03-01 21:36:38 +0700

    push dulu bingung dengan godot 4

--- src/ui/ui_menu.gd
+++ src/ui/ui_menu.gd
@@ -1,12 +1,16 @@
 extends Control
 
-
-@onready var color_rect: ColorRect = %ColorRect
-@onready var anim_
+#@onready var color_rect: ColorRect = %ColorRect
+@onready var anim_: AnimationPlayer = %anim
+# Declare a variable to store a reference to the ColorRect node
+var color_rect
+var transition
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
-
+	# Use get_node to retrieve a reference to the ColorRect node
+	color_rect = get_node("ColorRect")
+	color_rect.visible = true
+	anim_.play("fade_in")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
