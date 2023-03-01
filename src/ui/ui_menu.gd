d6e2bcc7b8f2d19b847e52459b031f8bbc0a2681
Hanif012 <andque12345012@gmail.com>
2023-03-01 21:04:43 +0700

    conflict :(

---
+++ src/ui/ui_menu.gd
@@ -0,0 +1,12 @@
+extends Control
+
+@onready var color_rect: ColorRect = %ColorRect
+@onready var anim_
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
