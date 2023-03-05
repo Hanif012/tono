3df8ed18b966a789bbb9039f20d493af434a9347
Hanif012 <andque12345012@gmail.com>
2023-03-06 03:40:29 +0700

    add and testing some 3d, still need 3d movement tho

--- src/level/player/player.gd
+++ src/level/player/player.gd
@@ -31,11 +31,3 @@ func _physics_process(delta):
 		velocity.z = move_toward(velocity.z, 0, SPEED)
 
 	move_and_slide()
-	if Input.is_action_just_pressed("ui_cancel"): 
-		Input.mouse_mode = Input.MOUSE_MODE_CAPTURED if Input.mouse_mode == Input.MOUSE_MODE_VISIBLE else Input.MOUSE_MODE_VISIBLE
-
-func _input(event):
-	if event is InputEventMouseMotion:
-		rotate_y(-event.relative.x * look_sensitivity)
-		camera.rotate_x(-event.relative.y * look_sensitivity)
-		camera.rotation.x = clamp(camera.rotation.x, -PI/2, PI/2)
