50fd1a0458ba7aa80e12c27c816472a531c49a53
Hanif012 <andque12345012@gmail.com>
2023-03-06 21:08:53 +0700

    dup level might just back to 2d shit

--- src/level/player/player.gd
+++ src/level/player/player.gd
@@ -1,6 +1,5 @@
 extends CharacterBody3D
 
-@onready var camera = get_node("camera3D")
 
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
@@ -20,7 +19,6 @@ func _physics_process(delta):
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
@@ -31,3 +29,4 @@ func _physics_process(delta):
 		velocity.z = move_toward(velocity.z, 0, SPEED)
 
 	move_and_slide()
+	
