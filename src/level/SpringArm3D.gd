07831788fab94c464c69661710612524816df3d3
Hanif012 <andque12345012@gmail.com>
2023-03-28 22:35:50 +0700

    fix cam

--- src/level/SpringArm3D.gd
+++ src/level/SpringArm3D.gd
@@ -1,7 +1,7 @@
-extends SpringArm3D
+extends Node3D
 
 @export var target_node: Node3D
 
 func _process(delta):
 	if target_node != null:
-		set_position(target_node.get_global_position())
+		set_position(target_node.get_position())
--- src/level/ball.gd
+++ src/level/ball.gd
@@ -1,7 +1,6 @@
 extends RigidBody3D
 
-var force = 40
-var rotate_force = 20
+var force = 50
 
 func _ready():
 	pass
@@ -13,14 +12,10 @@ func _physics_process(delta):
 #	)
 
 	if Input.is_action_pressed("ui_up"):
-		apply_central_force(Vector3(20, 0, 0))
-#	elif Input.is_action_pressed("ui_down"):
-#		self.angular_velocity.x += force*delta
+		self.angular_velocity.x -= force*delta
+	elif Input.is_action_pressed("ui_down"):
+		self.angular_velocity.x += force*delta
 	if Input.is_action_pressed("ui_left"):
 		self.angular_velocity.z += force*delta
 	elif Input.is_action_pressed("ui_right"):
 		self.angular_velocity.z -= force*delta
-
-	if Input.is_action_pressed("left_awsd"):
-		self.rotation_degrees.y -= delta*rotate_force
-
