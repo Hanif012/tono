9b10e476e60cb075ed56b4f5b11e46684686cf74
Hanif012 <andque12345012@gmail.com>
2023-04-15 13:35:48 +0700

    map, plat, finish, fall area

--- src/autoload/Global.gd
+++ src/autoload/Global.gd
@@ -3,8 +3,21 @@ extends Node
 #game system___________________
 var transition: bool     = false
 var pause     : bool     = false
+
 #level_________________________
-var level_unlocked       
+var level_list = [
+	"res://src/level/level_1.tscn",
+	"res://src/level/level_2.tscn"
+	
+]
+
+var level_index = 0  
+func load_next_level():
+	level_index += 1
+	level_index %= level_list.size()
+	get_tree().call_group("instanced", "queue_free")
+	get_tree().change_scene(level_list[level_index])
+
 var coin
 
 #music_________________________
